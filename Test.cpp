196dd275f2fd0383e52017665f9706a02f923cd1
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-07 22:27:48 +0530

    Refactor output statements in Test.cpp to improve clarity

--- Test.cpp
+++ Test.cpp
@@ -2,6 +2,7 @@
 
 int main() 
 {
-    std::cout << "VS Code is ready for C++!" << std::endl;
+    std::cout<< "VS Code is ready for C++!" << std::endl;
+    std::cout<< "Add two numbers: "<< (5 + 3) << std::endl;
     return 0;
 }

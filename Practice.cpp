20a1c423beea630a3f77495fcf063753caf88ef6
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-08 12:57:45 +0530

    Add Practice.cpp with initial comments outlining program functionality

---
+++ Practice.cpp
@@ -0,0 +1,8 @@
+/***********************************************************
+ * 1. Take 5 positive integers from user
+ * 2. Calculate the average
+ * 3. Calculate the summation
+ * 4. Find the largest and smallest
+ * 5. Find the difference between largest and the smallest
+ ************************************************************/
+

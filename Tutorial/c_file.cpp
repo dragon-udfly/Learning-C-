48b0aa3861a85185e5692edcecea0bde6e299328
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-08 14:01:12 +0530

    Add c_file.cpp with calculations for PI and days in a week

---
+++ Tutorial/c_file.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main() {
+
+    const double kPiValue= 3.14;
+    const int kDayInAWeek= 7;
+
+    std::printf("Value of PI: %.2f\n", kPiValue);
+    std::printf("Number of days in a week: %d\n", kDayInAWeek);
+
+    return 0;
+}

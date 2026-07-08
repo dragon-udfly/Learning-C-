81716fd51994c0cf04014aae22e9421ee24677c8
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-08 20:31:22 +0530

    Add program_one.cpp to demonstrate user information display using printf

---
+++ Tutorial/program_one.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <string>
+
+typedef std::string Text;
+
+int main() {
+    Text username= "user23";
+    Text password= "83dkce";
+    Text country= "India";
+    Text city= "New Delhi";
+
+    std::printf("User Name: %s\n", username.c_str());
+    std::printf("Password: %s\n", password.c_str());
+    std::printf("Country: %s\n", country.c_str());
+    std::printf("City: %s\n", city.c_str());
+    
+    return 0;
+}

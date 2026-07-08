b8bd8466b6bf528bb1830eb1982e3ebdcc5ec2d7
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-08 21:42:35 +0530

    Add mathematical functions and summary output to math_functions.cpp

--- Tutorial/math_functions.cpp
+++ Tutorial/math_functions.cpp
@@ -1,6 +1,20 @@
 #include <cstdio>
+#include <cmath>
 
 int main() {
+
+    int power_of_two = std::pow(4, 2);
+    int sqrt_of_value = std::sqrt(9);
+    int abs_of_value = std::abs(-35);
+    int ceil_of_value = std::ceil(45/6);
+    int floor_of_value = std::floor(34/8);
+
+    std::printf("------Summary-------\n");
+    std::printf("Power: %d\n", power_of_two);
+    std::printf("Square Root: %d\n", sqrt_of_value);
+    std::printf("Absolute Value: %d\n", abs_of_value);
+    std::printf("Ceil Value: %d\n", ceil_of_value);
+    std::printf("Floor Value: %d\n", floor_of_value);
 
     return 0;
 }

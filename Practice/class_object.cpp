ba68681938e243a28e0c3576332ac17426dd2b32
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-14 22:47:46 +0530

    Add implementation of OrangeClass with main function

---
+++ Practice/class_object.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <string>
+
+class OrangeClass {
+    public:
+        OrangeClass() = default;
+        OrangeClass(const std::string& name, int oranges)
+            : og_name_(name), oranges_(oranges) {
+            std::printf("Orange Class Is Initialized.\n");
+        }
+
+        std::string GetName() const {return og_name_;}
+        
+        void SetName(const std::string& name) {
+            og_name_ = name;
+        }
+
+        int GetOranges() const {return oranges_;}
+
+        void SetOranges(int oranges) {
+            oranges_ = oranges;
+        }
+
+        void PrintStuff() const {
+            std::printf("Name: %s, Oranges: %d\n", og_name_.c_str(), oranges_);
+        }
+
+    private:
+        int oranges_ = 0;
+        std::string og_name_ = "";
+};
+int main() {
+
+    OrangeClass orange_one("StoneCold", 32);
+
+    orange_one.PrintStuff();
+
+    return 0;
+}

a67b3205f3326f7aaf1973f44ba785e445ad2b8d
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-22 10:54:10 +0530

    Check whether pointer is NULL

--- Practice/pointer.cpp
+++ Practice/pointer.cpp
@@ -6,5 +6,17 @@ int main() {
 
     std::printf("Value: %d, Address: %x\n", value, &value);
 
+    int *ptr_get = NULL;
+
+    if(!ptr_get) {
+        std::printf("The pointer is NULL. %x\n", ptr_get);
+        ptr_get = &value;
+    }
+
+    if(ptr_get) {
+        std::printf("The pointer is NOT NULL.%x\n", ptr_get);
+        ptr_get = NULL;
+    }
+
     return 0;
 }

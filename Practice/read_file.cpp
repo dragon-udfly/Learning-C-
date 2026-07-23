a9190aa633ef98612aae039b34c2d920f78838c4
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-23 22:08:10 +0530

    Created file object and buffer

---
+++ Practice/read_file.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <string>
+#include <cstdlib>
+
+int main() {
+
+    // reading a file 
+    std::FILE* file_object = fopen("file.txt", "r");
+    std::string buffer;
+
+    return 0;
+}

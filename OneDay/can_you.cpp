3db22331285d88b239923a900fdc606a189f9d7f
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-08-09 22:23:17 +0530

    Refactor main function to use printf for argument display instead of cout

--- OneDay/can_you.cpp
+++ OneDay/can_you.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
+#include <cstdio>
 
-int main(int argc, char** argv){
+int main(int argc, char* argv[]) {
 
-    std::cout<< "Hi There!"<< std::endl;
-
+    std::printf("Argument Count: %d\n", argc);
+    for(int i = 0; i < argc; i++) {
+        std::printf("%s\n", argv[i]);
+    }
     return 0;
 }

c37d7060671aa5618600fa77f22fac4af06317bd
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-09-23 21:23:42 +0530

    Add Book class implementation with constructors, getters, and setters

--- Class/class_file.cpp
+++ Class/class_file.cpp
@@ -1,4 +1,31 @@
 #include <cstdio>
+#include <string>
+
+class Book{ 
+    public: 
+        Book() = default; // default constructor
+
+        // parameter constructor 
+        // initializing class variable during object creations
+        Book(const std::string& title, const std::string& author, double price) 
+        :title_(title), author_(author), price_(price){}
+
+        // getters 
+
+        std::string GetTitle() const {return title_;}
+        std::string GetAuthor() const {return author_;}
+        double GetPrice() const {return price_;}
+
+        // setters 
+        void SetTitle(const std::string& title) {title_ = title;}
+        void SetAuthor(const std::string& author) {author_ = author;}
+        void SetPrice(double price) {price_ = price;}
+
+    private: 
+        std::string title_ = "unknown"; 
+        std::string author_ = "unknown"; 
+        double price_ = 0.0; 
+};
 
 int main(int argc, char* argv[]){
 
